01cb3f1e90d79ece073b7ba5447e84f3eadbce8b
wizard <kwargs@gmail.com>
2009-11-16 16:02:44 +0300

    ++doc

--- mpd_cover.lua
+++ mpd_cover.lua
@@ -1,4 +1,15 @@
-
+--
+-- How to use:
+-- In the settings section (before TEXT), add this:
+--     lua_load /path/to/mpd_cover.lua
+-- modify mpd_cover.lua:
+--     change library_path
+--     sonata_cover_path (set it to nil if you don't use sonata)
+--     no_cover_path (image that is shown if cover not found)
+-- add in TEXT section something like:
+--
+-- ${if_mpd_playing}${lua_parse mpd_get_cover}${endif}
+--
 require "lfs"
 
 library_path = '/home/wizard/music/'
